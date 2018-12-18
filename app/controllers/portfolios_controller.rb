217c6deec921f713674acf6c0c9c631a339797fa
ajeffika <jan.mistarz6767@gmail.com>
2018-12-18 14:59:52 +0100

    added edit funciontality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,11 +1,11 @@
 class PortfoliosController < ApplicationController
 
   def index
-    @portfolio_items=Portfolio.all
+    @portfolio_items= Portfolio.all
   end
 
   def new
-    @portfolio_item=Portfolio.new
+    @portfolio_item= Portfolio.new
   end
 
   def create
@@ -19,6 +19,22 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def edit
+    @portfolio_item= Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item= Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
   def portfolio_params
     params.require(:portfolio).permit(:title,:subtitle,:body)
 end
