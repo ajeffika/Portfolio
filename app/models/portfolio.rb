b19f4726980af3e293c92c15ed2c46563f38a416
ajeffika <jan.mistarz6767@gmail.com>
2019-01-22 12:28:52 +0100

    installed jquery ui, sortable html5 ad css calls

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -11,6 +11,10 @@ class Portfolio < ApplicationRecord
   end
   scope :ruby_on_rails, ->{where(subtitle: 'Ruby on rails')}
 
+  def self.by_position
+    order('position ASC')
+  end
+
   after_initialize :set_defaults
   def set_defaults
     self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
