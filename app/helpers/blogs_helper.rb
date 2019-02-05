0cdd301e944db7f2c7d4e4587a495ff59393f023
ajeffika <jan.mistarz6767@gmail.com>
2019-02-05 12:13:05 +0100

    integrated custom fonts

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -9,19 +9,22 @@ module BlogsHelper
       CodeRay.scan(code, language).div
     end
   end
+
   def markdown(text)
-    coderayified= CodeRayify.new(filter_html: true, hard_wrap: true)
-    options= {
-        fenced_code_blocks: true,
-        no_intra_emphasis: true,
-        autolink: true,
-        lax_html_blocks: true,
+    coderayified = CodeRayify.new(filter_html: true,  hard_wrap: true)
+    options = {
+        :fenced_code_blocks => true,
+        :no_intra_emphasis => true,
+        :autolink => true,
+        :strikethrough => true,
+        :lax_html_blocks => true,
+        :superscript => true
     }
     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
     markdown_to_html.render(text).html_safe
   end
+
   def blog_status_color blog
     'color: red;' if blog.draft?
   end
-
 end
