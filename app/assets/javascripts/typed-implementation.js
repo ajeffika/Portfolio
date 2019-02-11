09bc146ea7806d375a172f999f1ac34888a5615c
ajeffika <jan.mistarz6767@gmail.com>
2019-02-11 13:07:53 +0100

    implemented a workaround for buggy typed-js library

---
+++ app/assets/javascripts/typed-implementation.js
@@ -0,0 +1,10 @@
+document.addEventListener("turbolinks:load", function() {
+
+var options = {
+
+    strings: ['','Hello My name is jeff.', 'Jeffofsky, Jeff Jeffofsky'],
+    typeSpeed: 40
+}
+
+var typed = new Typed(".element", options);
+});
