fc31106baf9174c095439ca9092e0b85ea335411
ajeffika <jan.mistarz6767@gmail.com>
2019-02-21 12:46:18 +0100

    refactored typed-js and translations

--- app/assets/javascripts/typed-implementation.js
+++ app/assets/javascripts/typed-implementation.js
@@ -2,8 +2,10 @@ document.addEventListener("turbolinks:load", function() {
 
 var options = {
 
-    strings: ['',I18n.t('sentence1'), I18n.t('sentence2')],
-    typeSpeed: 30
+    strings: ['',I18n.t('sentence1'), I18n.t('sentence2'), I18n.t('sentence3')],
+    typeSpeed: 45,
+    backSpeed: 15,
+    smartBackspace: true
 };
 
 var typed = new Typed(".element", options);
